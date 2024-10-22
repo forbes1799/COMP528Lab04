e32be5eb49656fb88c1c4119b3163017275c9d69
forbes1799 <123934016+forbes1799@users.noreply.github.com>
2024-10-22 15:15:45 +0100

    Add files via upload

---
+++ slurm_tmp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH -p course
+#SBATCH -D ./
+#SBATCH -t 3
+#SBATCH --export=ALL
+
+cores=${SLURM_CPUS_PER_TASK:-1} #Gets the requested number of cores when sbatch -c is used
+
+export OMP_NUM_THREADS=$cores   #Exports the OMP_NUM_THREADS variable
+
+# 1. Add code here to compile a certain program
+
+# 2. Add code here to run the program
+
+# 3. Then submit this script to the compute nodes. 
+#    Instructions for how to do this are in the lab
